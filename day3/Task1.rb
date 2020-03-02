c173222dd30db099eb46f3eb06399be5da233a8c
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-03-02 12:23:24 +0530

    Correct My indentation in Task.rb

--- day3/Task1.rb
+++ day3/Task1.rb
@@ -1,9 +1,14 @@
-first = Proc.new{|n,v| puts "#{n}  #{v}"
-                     n+v    
-                }
-second = -> (n,v){puts "#{n}  #{v}" 
-                  n+v
-                    }
+first = Proc.new do |n,v|  
+  puts "#{n}  #{v}"  
+  n+v  
+end
+
+second = lambda do |n,v|
+  puts "#{n}  #{v}" 
+    n+v
+end
+
+puts ""
 print second.call(2,7)
 print first.call(2,8,5,6,)
 
